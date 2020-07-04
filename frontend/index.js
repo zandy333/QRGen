8b918681844ba2edb369b0129ff605f7a47c2d5e
Sandamal Siripathi <sndml@yahoo.com>
2020-07-04 05:51:29 +0530

    First commit

--- frontend/index.js
+++ frontend/index.js
@@ -1,98 +1,80 @@
-import {initializeBlock, useBase, useLoadable, useWatchable, useRecordById, useRecordIds} from '@airtable/blocks/ui';
+import {initializeBlock, useBase, useLoadable, useWatchable, useRecordById} from '@airtable/blocks/ui';
 import React, {useState} from 'react';
 import {cursor} from '@airtable/blocks';
 import ReactDOM from 'react-dom';
 
-var QRCode = require('qrcode.react'); // Add the external QR Code library (https://github.com/zpao/qrcode.react)
-			// This third-party library is licensed under ISC License (https://github.com/zpao/qrcode.react/blob/master/LICENSE)
-			// This license allows commercial usage without any restrictions, for the QR Code library used in this block
+
+// Add the external QR Code rendering library (https://github.com/zpao/qrcode.react)
+// This third-party library is licensed under ISC License (https://github.com/zpao/qrcode.react/blob/master/LICENSE)
+// This license allows commercial usage without any restrictions, for the QR Code rendering library used in this block
+var QRCode = require('qrcode.react'); 
+
 
 function QRGen() {
   
-  	const MAX_CAPACITY = 2001; // Maximum Data Capacity Limit Setting for the QR Code to be displayed
+  	// Maximum Data Capacity Limit Setting for the QR Code to be displayed
+  	const MAX_CAPACITY = 2001; 
 	
-	const base = useBase(); // Get a reference to the base
+	// Get a reference to the base
+	const base = useBase(); 
 	
-	var currentTable = base.getTableByIdIfExists(cursor.activeTableId); // Get a reference to the active table
-	var field = null;
-	var recordIds = null;
-	var record = null;
-	var cellval = "";
-	var text = "";
-	var qrstyle = null;
-	var textstyle = null;
-	
+	// Set up watchables
 	useLoadable(cursor); 
-	useWatchable(cursor, ['activeTableId', 'selectedFieldIds', 'selectedRecordIds']); // Set up watchables	
+	useWatchable(cursor, ['activeTableId', 'selectedFieldIds', 'selectedRecordIds']); 
 	
-	if (currentTable != null) { // If currentTable is NOT null (this may momentarily become null just when a new table is created)
+	// Obtain a reference to the active table
+	const table = base.getTableByIdIfExists(cursor.activeTableId); 
 	
-		if (cursor.selectedFieldIds[0] == null) { // If no cell is selected on the table
+	// If table is not null
+	const currentTable = table ? table : null;
+	
+	// If currentTable is not null
+	const field = currentTable ? currentTable.getFieldByIdIfExists(cursor.selectedFieldIds[0]) : null;
+	
+	// If both currentTable and field are not null
+	const record = currentTable && field ? useRecordById(currentTable, cursor.selectedRecordIds[0]) : useRecordById(null, null);
+	
+	// If only one cell on the table is selected
+	const onlyOneCellSelected = ((cursor.selectedFieldIds.length == 1) && (cursor.selectedRecordIds.length == 1)) ? true : false;
+	
+	// If more than one cell on the table are selected
+	const severalCellsSelected = ((cursor.selectedFieldIds.length > 1) || (cursor.selectedRecordIds.length > 1)) ? true : false;
+	
+	// Obtain a reference to the selected table cell contents' length (size)
+	const cellLength = record? record.getCellValueAsString(cursor.selectedFieldIds[0]).length : null;
+	
+	// If all currentTable, record and field are not null
+	// only one cell is selected on the table
+	// table cell contents are within the maximum limit 
+	const cellval = currentTable && record && field && onlyOneCellSelected && (cellLength < MAX_CAPACITY) ? record.getCellValueAsString(cursor.selectedFieldIds[0]) : "";
+	
+	// If a record was deleted via right-click menu while the cell is selected
+	const recDeletedByRightClick = (!field || !record) ? true : false;
 		
-			field = currentTable.getFieldIfExists("");
-			recordIds = useRecordIds(null);
-			record = useRecordById(null, null);
-			cellval = "";
-			text = "Select a table cell.";
-			qrstyle = {backgroundColor: "#fff", position: "absolute", display: "none", padding: "5%", left: "5%", top: "5%", 
+	// Handle the following five cases using a nested ternary expression
+	// case 1 : if only one cell is selected 
+	// case 2 : if several cells are seleceted
+	// case 3 : if no cell is selected
+	// case 4 : if a record was deleted via right-click menu while the cell is selected
+	// case 5 : if selected table cell contents are greater than (or less than) the pre-set maximum limit
+	const text = (cellLength >= MAX_CAPACITY && onlyOneCellSelected && !recDeletedByRightClick) ? "Cell contents are too large to fit in a QR Code." : onlyOneCellSelected && !recDeletedByRightClick ? "" : severalCellsSelected && !recDeletedByRightClick ? "Select only one cell." : "Select a table cell.";
+	
+	// Settings for showing the QR Code
+	const showQR = {backgroundColor: "#fff", position: "absolute", display: "block", padding: "5%", left: "5%", top: "5%", 
 			width: "90%", height: "auto"};
 			
-		} else { // If some cell is selected on the table
+	// Settings for hiding the QR Code
+	const hideQR = {backgroundColor: "#fff", position: "absolute", display: "none", padding: "5%", left: "5%", top: "5%", 
+			width: "90%", height: "auto"};
 		
-			field = currentTable.getFieldByIdIfExists(cursor.selectedFieldIds[0]);
-			recordIds = useRecordIds(currentTable);
-			record = useRecordById(currentTable, cursor.selectedRecordIds[0]);
-			
-			if ((cursor.selectedFieldIds.length == 1) && (cursor.selectedRecordIds.length == 1) && (record != null)) { // Make sure no multiple cells are selected, and handle the error if a record was deleted via right click menu, while the cell is still selected
-			 
-				cellval = record.getCellValueAsString(cursor.selectedFieldIds[0]);
-				text = "";
-				qrstyle = {backgroundColor: "#fff", position: "absolute", display: "block", padding: "5%", left: "5%", top: "5%", 
-				width: "90%", height: "auto"};
-				
-			} else if (((cursor.selectedFieldIds.length != 1) || (cursor.selectedRecordIds.length != 1)) && (record != null)) { // If more than one cells are selected
-				cellval = "";
-				text = "Select only one cell.";
-				qrstyle = {backgroundColor: "#fff", position: "absolute", display: "none", padding: "5%", left: "5%", top: "5%", 
-				width: "90%", height: "auto"};
-				
-			} else if (record == null) { // If the record containing the selected cell was deleted via right click menu
+	// Decide whether to show or hide the QR Code		
+	const qrstyle = onlyOneCellSelected && !recDeletedByRightClick && cellLength < MAX_CAPACITY ? showQR : hideQR;
+	
+	// Settings for text
+	const textstyle = {textAlign: "center", display: "block"};
 
-				cellval = "";
-				text = "Select a table cell.";
-				qrstyle = {backgroundColor: "#fff", position: "absolute", display: "none", padding: "5%", left: "5%", top: "5%", 
-				width: "90%", height: "auto"};			
-			}
-			
-		}
-	
-	} else { // If currentTable is null (just when a new table is created)
-		
-		field = null;
-		recordIds = useRecordIds(null);
-		record = useRecordById(null, null);
-		cellval = "";
-		text = "Table is empty.";
-		qrstyle = {backgroundColor: "#fff", position: "absolute", display: "none", padding: "5%", left: "5%", top: "5%", 
-			width: "90%", height: "auto"};
-		
-	}
-	
-	
-	if (cellval.length < MAX_CAPACITY) { // If the cell contents are within the maximum capacity limit of a QR Code
-		cellval = cellval;
-		
-	} else { // If the cell contents exceed the maximum capacity limit of a QR Code
-		cellval = "";
-		text = "Cell contents are too large to fit in a QR Code.";
-		qrstyle = {backgroundColor: "#fff", position: "absolute", display: "none", padding: "5%", left: "5%", top: "5%", 
-			width: "90%", height: "auto"};
-	}
-	
-	
-	textstyle = {textAlign: "center", display: "block"};
 
-	return ( // Display the text message and QR Code accordingly via the imported third-party QR Code library
+	return ( // Selectively display the text message or the QR Code via the imported third-party QR Code library
 		<> 
 		<p style={textstyle}>{text}</p>
 		<QRCode value={cellval} style={qrstyle} renderAs="svg" />	
@@ -101,3 +83,5 @@ function QRGen() {
 }
 
 initializeBlock(() => <QRGen />);
+
+
